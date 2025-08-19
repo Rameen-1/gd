208347311bcb5a7c96724a060930a53352adc9f7
Rameen Zeeshan <rameen.zeeshanedu@gmail.com>
2025-08-19 18:12:42 +0400

    Create thankyou.php

---
+++ thankyou.php
@@ -0,0 +1,9 @@
+<?php include 'header.php'; $_SESSION['cart'] = []; ?>
+<main>
+    <h2>Thank You!</h2>
+    <p>Your domains have been reserved (mock process). You'll receive an email shortly.</p>
+    <a href="index.php">Search More</a>
+</main>
+</body>
+</html>
+ 
