38070ea03aaa83f55aba5257bafe593cab30dabf
Rameen Zeeshan <rameen.zeeshanedu@gmail.com>
2025-08-19 18:08:58 +0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include 'header.php'; ?>
+<main>
+    <h2>Find Your Perfect Domain</h2>
+    <form action="search.php" method="GET">
+        <input type="text" name="domain" placeholder="Enter domain name" required>
+        <button type="submit">Search</button>
+    </form>
+</main>
+</body>
+</html>
+ 
